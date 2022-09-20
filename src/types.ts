188b5174a222697c76fe90d31a8c13c12d5ef272
Bruno Aiolfi <bruno.aiolfi154@gmail.com>
2022-09-20 11:52:54 -0300

    Add files via upload

---
+++ src/types.ts
@@ -0,0 +1,19 @@
+export type CreateSessionDTO = {
+  email: string;
+  password: string;
+}
+
+type UserData = {
+  password: string;
+  permissions: string[];
+  roles: string[];
+  name: string;
+}
+
+export type UsersStore = Map<string, UserData>
+
+export type RefreshTokensStore = Map<string, string[]>
+
+export type DecodedToken = {
+  sub: string;
+}
